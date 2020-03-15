a5106ba612df32a2c01e21b5f55c232a69b4e8a3
Paul Borm <mail@paulborm.de>
2020-03-15 15:20:39 +0100

    Update server
    - exclude sounds
    - handle different events
    - stream audio chunks to client

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
+const fs = require("fs");
+const sounds = require("./sounds");
 
 const app = express();
 const server = http.createServer(app);
@@ -12,11 +14,42 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+let state = {
+  users: []
+};
+
 io.on("connection", socket => {
   console.log(`a user connected: ${socket.id}`);
 
+  state = {
+    ...state,
+    users: [...state.users, socket.id]
+  };
+
+  io.emit("user", { amount: state.users.length });
+
+  socket.on("sound", data => {
+    console.log("sound", data);
+    const sound = sounds.filter(({ id }) => id === data.soundId)[0];
+    const audioReadStream = fs.createReadStream(sound.path, {
+      encoding: "binary",
+      highWaterMark: 128 * 1024
+    });
+
+    audioReadStream.on("data", function(chunk) {
+      console.log(`Streaming sound: ${data.soundId}`);
+      io.emit("sound", { chunk });
+    });
+  });
+
   socket.on("disconnect", () => {
     console.log(`user disconnected: ${socket.id}`);
+    const filteredUsers = state.users.filter(user => user !== socket.id);
+    state = {
+      ...state,
+      users: [...filteredUsers]
+    };
+    io.emit("user", { amount: state.users.length });
   });
 });
 
