c5c0da11eb77f16efbe5ce1ba81f4a3c5dd91472
Paul Borm <mail@paulborm.de>
2020-03-21 15:14:47 +0100

    Create useSounds hook

---
+++ src/hooks/useSounds.js
@@ -0,0 +1,52 @@
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  status: "idle",
+  data: []
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return {
+        ...state,
+        status: "loading"
+      };
+    case "success":
+      return {
+        ...state,
+        status: "success",
+        data: action.payload
+      };
+    case "error":
+    default:
+      return {
+        ...state,
+        status: "error"
+      };
+  }
+}
+
+const useSounds = () => {
+  const [state, dispath] = useReducer(reducer, initialState);
+  const { status, data } = state;
+
+  useEffect(() => {
+    dispath({ type: "loading" });
+    fetch("http://localhost:3001/api/sounds")
+      .then(response => response.json())
+      .then(json => {
+        dispath({ type: "success", payload: json });
+      })
+      .catch(error => {
+        dispath({ type: "error", payload: error });
+      });
+  }, []);
+
+  return {
+    status,
+    data
+  };
+};
+
+export default useSounds;
