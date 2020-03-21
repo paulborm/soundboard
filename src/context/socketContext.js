717b86c660b73fe91b7e505c8fb68fd69a7100a2
Paul Borm <mail@paulborm.de>
2020-03-21 15:13:56 +0100

    Move socket.io initializiation into SocketProvider

--- src/context/socketContext.js
+++ src/context/socketContext.js
@@ -1,15 +1,12 @@
 import React, { createContext } from "react";
 import io from "socket.io-client";
 
-const socket = io(process.env.API_URL || "http://localhost:3001");
-
 export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
+  const socket = io(process.env.API_URL || "http://localhost:3001");
 
   return (
-    <SocketContext.Provider value={socket}>
-      {children}
-    </SocketContext.Provider>
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
   );
 };
