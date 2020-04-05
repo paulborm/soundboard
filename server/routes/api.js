49ba569148da7077386193fe8dbb2243f4227207
Paul Borm <mail@paulborm.de>
2020-04-05 16:24:07 +0200

    Remove ./server directory
    Move to it's own repository

--- server/routes/api.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use("/sounds", require("../api/sounds"));
-
-module.exports = router;
--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const express = require("express");
-const router = express.Router();
-
-router.use("/", (_req, res) => {
-  res.sendFile("public/index.html", { root: path.dirname(__dirname) });
-});
-
-module.exports = router;
